dc453a73291e32c800074036dc59c3356fb45f6f
henelngata <bucketpena013@gmail.com>
2024-08-14 20:45:40 +0300

    Top

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,17 +11,22 @@ def top_ten(subreddit):
     subreddit = str
     prints title
     """
-    url = ("https://api.reddit.com/r/{}?sort=hot&limit=10".format(subreddit))
-    headers = {'User-Agent': 'CustomClient/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'Mozilla/5.0'}
 
-    if response.status_code != 200:
-        print(None)
-        return
-    response = response.json()
-    if 'data' in response:
-        for posts in response.get('data').get('children'):
-            print(posts.get('data').get('title'))
-
-    else:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data['data']['children']
+            if not posts:
+                print(None)
+                return
+            for post in posts:
+                print(post['data']['title'])
+        elif response.status_code in [301, 404]:
+            print(None)
+        else:
+            print(None)
+    except requests.exceptions.RequestException:
         print(None)
