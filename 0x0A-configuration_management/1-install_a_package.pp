c548e5ab6bb93e2adcb6ba2f72957d6e847cce2b
henelngata <bucketpena013@gmail.com>
2024-08-16 15:57:13 +0300

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,9 +3,9 @@ package { 'python3-pip':
   ensure => installed,
 }
 
-# Install Flask version 2.1.0 using pip3
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+# Install specific versions of Flask and Werkzeug to avoid compatibility issues
+exec { 'install_flask_and_werkzeug':
+  command => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.0.3',
   unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
   require => Package['python3-pip'],
 }
